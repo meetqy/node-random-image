8a9896fa774ca83623a821a0ff200054c13daaf0
xiaoqiyang <meetqy@qq.com>
2022-06-04 12:42:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3001;
+
+app.use(express.static("public"));
+
+const _assetsJson = assetsJson();
+console.log(_assetsJson);
+
+app.get("/api/:type", (req, res) => {
+  const type = req.params.type;
+  const max = _assetsJson[type];
+
+  if (!max) {
+    res.send("type 不存在");
+  }
+
+  res.redirect(302, `/${type}/${Random(0, max)}.jpg`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+function Random(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+function assetsJson() {
+  const res = fs.readdirSync("./public");
+  let json = {};
+  res.map((item) => {
+    const dir = "./public/" + item;
+    if (fs.lstatSync(dir).isDirectory()) {
+      json[item] = fs.readdirSync(dir).length;
+    }
+  });
+
+  return json;
+}
